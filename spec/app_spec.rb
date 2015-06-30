8c65765d6d8b831a1b070301729bc71bfb544309
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-06-30 16:58:59 +0900

    [add] refs #4 レスポンスに関するテストを記述する

---
+++ spec/app_spec.rb
@@ -0,0 +1,47 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "App" do
+  include Rack::Test::Methods
+
+  def app
+    @app ||= Sinatra::Application
+  end
+
+  describe "レスポンスの精査" do
+    describe "GET /" do
+      subject { get '/' }
+      it "正常なレスポンスが返ること" do
+        expect(subject).to be_ok
+      end
+    end
+
+    describe "GET /api/battle" do
+      subject { get '/api/battle' }
+      it "正常なレスポンスが返ること" do
+        expect(subject).to be_ok
+      end
+    end
+
+    describe "GET /assets/js/send_form.js" do
+      subject { get '/assets/js/send_form.js' }
+      it "正常なレスポンスが返ること" do
+        expect(subject).to be_ok
+      end
+    end
+
+    describe "GET /assets/js/game.js" do
+      subject { get '/assets/js/game.js' }
+      it "正常なレスポンスが返ること" do
+        expect(subject).to be_ok
+      end
+    end
+
+    describe "GET /assets/css/application.css" do
+      subject { get '/assets/css/application.css' }
+      it "正常なレスポンスが返ること" do
+        expect(subject).to be_ok
+      end
+    end
+  end
+end
+
