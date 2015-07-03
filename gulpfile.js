f464fdb14e4b9d500fbdc7c0c37012142d6140bd
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-07-03 16:55:47 +0900

    [sys] refs #14 gulpによるcoffeeのコンパイルを設定

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var plumber = require('gulp-plumber');
+
+var paths = {
+  scripts: 'src/scripts/*.coffee',
+  styles: 'src/styles/*.sass',
+  images: 'src/images/*'
+};
+
+var build_paths = {
+  scripts: 'public/assets/javascripts',
+  styles: 'public/assets/styles',
+  images: 'public/assets/images'
+};
+
+gulp.task('scripts', function() {
+  return gulp.src(paths.scripts)
+    .pipe(coffee())
+    .pipe(uglify())
+    .pipe(concat('all.min.js'))
+    .pipe(gulp.dest(build_paths.scripts));
+});
+
+gulp.task('default', ['scripts'])
+
