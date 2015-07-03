026845b6d6a630a159bbd32145a07d962e4caf4e
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-07-03 16:59:25 +0900

    [remove] refs #14 使用していないAPIを削除

--- app.rb
+++ app.rb
@@ -9,22 +9,6 @@ get '/api/battle' do
   "100,200|1\n100,100|0\n300,200|2"
 end
 
-get '/assets/js/send_form.js' do
-  coffee :'assets/js/send_form'
-end
-
-get '/assets/js/game.js' do
-  coffee :'assets/js/game'
-end
-
-get '/assets/js/turn.js' do
-  coffee :'assets/js/turn'
-end
-
-get '/assets/js/hand.js' do
-  coffee :'assets/js/hand'
-end
-
 get '/assets/css/application.css' do
   sass :'assets/css/application'
 end
