9626b014e4ac2078fdf45abc0a428196a14d8aa8
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-07-03 23:42:36 +0900

    [clean] ダブルではなくシングルクォートを使う

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,23 +1,23 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe "App" do
+describe 'App' do
   include Rack::Test::Methods
 
   def app
     @app ||= Sinatra::Application
   end
 
-  describe "レスポンスの精査" do
-    describe "GET /" do
+  describe 'レスポンスの精査' do
+    describe 'GET /' do
       subject { get '/' }
-      it "正常なレスポンスが返ること" do
+      it '正常なレスポンスが返ること' do
         expect(subject).to be_ok
       end
     end
 
-    describe "GET /api/battle" do
+    describe 'GET /api/battle' do
       subject { get '/api/battle' }
-      it "正常なレスポンスが返ること" do
+      it '正常なレスポンスが返ること' do
         expect(subject).to be_ok
       end
     end
