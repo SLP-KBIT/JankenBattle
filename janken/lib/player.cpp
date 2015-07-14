70d13d981ff4fce2884bf2e7047c9b859b51fade
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-07-14 18:59:18 +0900

    [add] refs #3 反則手でないか確認する

--- janken/lib/player.cpp
+++ janken/lib/player.cpp
@@ -14,5 +14,16 @@ Player::Player()
 void Player::_strategy(Player *enemy)
 {
   hand = strategy(_rock, _scissors, _paper, enemy->_rock, enemy->_scissors, enemy->_paper);
+  if ( ! can_use() ) { hand = NONE; }
+}
+
+bool Player::can_use()
+{
+  switch ( hand ) {
+    case ROCK : return _rock > 0;
+    case SCISSORS : return _scissors > 0;
+    case PAPER : return _paper > 0;
+    default : return false;
+  }
 }
 
--- janken/lib/player.h
+++ janken/lib/player.h
@@ -14,6 +14,8 @@ public:
 
   void _strategy(Player *enemy);
 
+  bool can_use();
+
   virtual Hand strategy(int rock, int scissors, int paper, int enemy_r, int enemy_s, int enemy_p) = 0;
 };
 
