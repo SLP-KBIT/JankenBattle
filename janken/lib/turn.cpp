85a56cfe585f2cfdac4307c96cec4fae8cf46222
gembaf <gembaf.github@gmail.com>
2015-07-14 20:06:09 +0900

    [add] refs #3 反則手を出したほうが負けるようにする

--- janken/lib/turn.cpp
+++ janken/lib/turn.cpp
@@ -24,6 +24,10 @@ void Turn::battle(Player *p1, Player *p2)
   p1->_strategy(p2);
   p2->_strategy(p1);
 
+  if ( p1->hand == NONE && p2->hand == NONE ) { winner = NULL; return; }
+  if ( p1->hand == NONE && p2->hand != NONE ) { winner = p2; return; }
+  if ( p1->hand != NONE && p2->hand == NONE ) { winner = p1; return; }
+
   if ( is_win(p1->hand, p2->hand) ) { winner = p1; }
   else if ( is_win(p2->hand, p1->hand) ) { winner = p2; }
   else { winner = NULL; }
@@ -43,6 +47,7 @@ bool Turn::is_fin(Player *p1, Player *p2)
 
 bool Turn::is_win(Hand hand1, Hand hand2)
 {
+  if ( hand1 == NONE ) { return false; }
   return hand1 == ROCK && hand2 == SCISSORS ||
     hand1 == SCISSORS && hand2 == PAPER ||
     hand1 == PAPER && hand2 == ROCK;
