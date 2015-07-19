82a87a78724e5f3278743c030160c2cf906ffce3
gembaf <gembaf.github@gmail.com>
2015-07-19 17:57:22 +0900

    [add] refs #27 提出ファイルから識別子以内のコードを抜き出す

--- lib/strategy.rb
+++ lib/strategy.rb
@@ -11,8 +11,23 @@ class Strategy
     end
   end
 
+  def slice
+    each_line = @text.split(/\r\n|\n/)
+    s = get_index_of_target(each_line, 'PLAYER_STRATEGY_START')
+    e = get_index_of_target(each_line, 'PLAYER_STRATEGY_END')
+    each_line.slice((s + 1)...e).join('\n')
+  end
+
   def exist?
     File.exist?(FILE_PATH)
+  end
+
+  private
+
+  def get_index_of_target(arr, target)
+    arr.each.with_index do |line, i|
+      return i if line =~ /#{target}/
+    end
   end
 end
 
--- spec/strategy_spec.rb
+++ spec/strategy_spec.rb
@@ -7,12 +7,30 @@ describe Strategy do
   include Rack::Test::Methods
 
   describe 'メソッドの動作確認' do
-    let(:strategy) { Strategy.new("hogehogehoge") }
+    before(:each) { strategy.write }
 
-    describe '#exists_strategy?' do
-      before(:each) { strategy.write }
+    let(:text) do
+      <<'EOS'
+hoge
+foo
+//--- PLAYER_STRATEGY_START
+piyo
+fuga
+//--- PLAYER_STRATEGY_END
+poge
+EOS
+    end
+    let(:strategy) { Strategy.new(text) }
+
+    describe '#exist?' do
       it 'ファイルの存在を確認できること' do
         expect(strategy.exist?).to eq true
+      end
+    end
+
+    describe '#slice' do
+      it '識別子以内の文字列を切り出せること' do
+        expect(strategy.slice).to eq 'piyo\nfuga'
       end
     end
   end
