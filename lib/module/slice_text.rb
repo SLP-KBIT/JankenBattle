1df78f9dcaa1cd46a67a670ea901eb3158dd3d5e
gembaf <gembaf.github@gmail.com>
2015-07-20 19:20:48 +0900

    [fix] refs #32 パターンにマッチしていない場合の処理を追加

--- lib/module/slice_text.rb
+++ lib/module/slice_text.rb
@@ -6,6 +6,9 @@ module SliceText
     header = each_line.slice(0..(s - 1))
     footer = each_line.slice((e + 1)..-1)
     [header, footer]
+  rescue => e
+    puts e
+    [each_line, nil]
   end
 
   def slice_within(text)
@@ -13,12 +16,16 @@ module SliceText
     s = get_index_of_target(each_line, 'PLAYER_STRATEGY_START')
     e = get_index_of_target(each_line, 'PLAYER_STRATEGY_END')
     each_line.slice((s + 1)...e)
+  rescue => e
+    puts e
+    each_line
   end
 
   def get_index_of_target(arr, target)
     arr.each.with_index do |line, i|
       return i if line =~ /#{target}/
     end
+    fail 'No Match Target'
   end
 
   module_function :slice_without, :slice_within
