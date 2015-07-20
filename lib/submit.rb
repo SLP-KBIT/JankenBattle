63f162f6f4d178480f35d4cb05bd870098de4ab4
gembaf <gembaf.github@gmail.com>
2015-07-20 19:22:11 +0900

    [add] refs #32 本物のslice_textを実装して使用する

--- lib/submit.rb
+++ lib/submit.rb
@@ -1,9 +1,17 @@
+require File.dirname(__FILE__) + '/module/slice_text'
+
 class Submit
+  include SliceText
+
   PATH = File.dirname(__FILE__) + '/../tmp/'
 
   def initialize(id, text)
     @text = text
     @path = PATH + "/strategy#{id}.c"
+  end
+
+  def slice_text
+    slice_within(@text)
   end
 
   def write
