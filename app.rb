fa42a4a2baaa23017c758226ba8092e15d7fe4db
gembaf <s15g464@stu.kagawa-u.ac.jp>
2015-07-21 15:30:59 +0900

    [add] refs #32 実際の実行結果をレスポンスとして返す

--- app.rb
+++ app.rb
@@ -3,12 +3,29 @@ require 'sinatra/reloader'
 
 require './lib/strategy'
 
+def convert_hand(hand)
+  return 'rock' if hand == 100
+  return 'scissors' if hand == 200
+  return 'paper' if hand == 300
+  'none'
+end
+
 get '/' do
   slim :index
 end
 
 get '/api/battle' do
-  "[{\"hand1\":\"paper\",\"hand2\":\"rock\"},{\"hand1\":\"rock\",\"hand2\":\"scissors\"}]"
+  `cd janken && make clean && make`
+  result_csv = `./janken/janken.exe`.split(/\r\n|\n/)
+  result = []
+  result_csv.each do |turn|
+    elem = turn.split(',').map(&:to_i)
+    hand1 = elem[0]
+    hand2 = elem[1]
+    # winner_id = elem[2]
+    result << { hand1: convert_hand(hand1), hand2: convert_hand(hand2) }
+  end
+  result.to_json
 end
 
 post '/api/upload/:id' do
